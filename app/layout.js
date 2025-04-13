70fd713975bae194f6616228039d647fed7b45fc
Vihananand <vihananand2018@gmail.com>
2025-04-14 00:45:28 +0530

    added analytics and spped insigths by vercel

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter, Cormorant_Upright } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const cormorant = Cormorant_Upright({
@@ -64,6 +66,8 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
